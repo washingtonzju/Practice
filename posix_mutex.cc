9e47b0a37103db80c8ddb697eecf7204765b7cf7
washingtonzju <zhongkai.mi@gmail.com>
2013-10-22 20:52:48 +0800

    posix added

---
+++ posix_mutex.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <map>
+using namespace std;
+
+map<string, string> g_pages;
+
+void save_page(const string &url)
+{
+}
+
+int main()
+{
+    
+    return 0;
+}
